3016503b675581673a9a7dc35b63e38ace9651c5
ibra-cium <siam242-15-767@diu.edu.bd>
2026-04-03 20:31:39 +0600

    dep error fixed

--- src/components/hero/CRTMonitor.tsx
+++ src/components/hero/CRTMonitor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 // Internal Matrix Rain Component
